b12ab2bda59a0e5ba9619c0a9505dc1b3b3877c2
Ashish Jayee <ashishjayee@gmail.com>
2017-08-11 00:09:57 -0700

    Removing old code

--- microservices_spring_web_mvc/src/main/java/com/niit/microservices/service/MessageRepositoryCustom.java
+++
@@ -1,8 +0,0 @@
-package com.niit.microservices.service;
-
-import com.niit.microservices.data.Message;
-
-public interface MessageRepositoryCustom {
-
-	public Message getByMessageID(Integer id);
-}
